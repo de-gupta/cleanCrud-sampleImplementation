f521a3a4090aadfa36cd6cbdd2e053ff8f1a9618
de-gupta <de-gupta@>
2026-03-23 11:23:53 +0100

    _updateBasicLibrary: updated libs, tests green

--- src/main/java/de/gupta/clean/crud/implementation/examples/task/useCases/crud/delete/infrastructure/persistence/service/TaskDeletePersistenceService.java
+++ src/main/java/de/gupta/clean/crud/implementation/examples/task/useCases/crud/delete/infrastructure/persistence/service/TaskDeletePersistenceService.java
@@ -1,9 +1,11 @@
 package de.gupta.clean.crud.implementation.examples.task.useCases.crud.delete.infrastructure.persistence.service;
 
+import de.gupta.clean.crud.implementation.examples.task.infrastructure.persistence.model.TaskPersistenceModel;
 import de.gupta.clean.crud.template.infrastructure.persistence.adapter.persistence.domain.id.adapter.DomainPersistenceIDAdapter;
 import de.gupta.clean.crud.template.useCases.crud.delete.application.service.DeletePersistenceService;
 import de.gupta.clean.crud.template.useCases.crud.delete.infrastructure.persistence.service.AbstractDeletePersistenceService;
 import de.gupta.clean.crud.template.useCases.crud.delete.infrastructure.persistence.service.DeletePersistenceModelRepository;
+import de.gupta.clean.crud.template.useCases.crud.fetch.infrastructure.persistence.service.FetchPersistenceModelRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +13,14 @@ import java.util.UUID;
 
 @Component
 @Qualifier("taskDeletePersistenceService")
-final class TaskDeletePersistenceService extends AbstractDeletePersistenceService<Long, UUID>
+final class TaskDeletePersistenceService extends AbstractDeletePersistenceService<Long, UUID, TaskPersistenceModel>
 		implements DeletePersistenceService<Long>
 {
 	TaskDeletePersistenceService(
-			@Qualifier("taskDeletePersistenceModelRepository") final DeletePersistenceModelRepository<UUID> repository,
+			final FetchPersistenceModelRepository<TaskPersistenceModel, UUID> fetchRepository,
+			@Qualifier("taskDeletePersistenceModelRepository") final DeletePersistenceModelRepository<UUID> deleteRepository,
 			@Qualifier("taskDomainPersistenceIDAdapter") final DomainPersistenceIDAdapter<Long, UUID> idAdapter)
 	{
-		super(repository, idAdapter);
+		super(fetchRepository, deleteRepository, idAdapter);
 	}
 }
