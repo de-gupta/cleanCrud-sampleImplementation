9d7ab2d14cde5862a072ba9a3f06a09115de7314
de-gupta <de-gupta@>
2026-03-25 12:07:26 +0100

    update pom

--- src/main/java/de/gupta/clean/crud/implementation/examples/task/useCases/crud/save/application/service/TaskSaveService.java
+++ src/main/java/de/gupta/clean/crud/implementation/examples/task/useCases/crud/save/application/service/TaskSaveService.java
@@ -6,7 +6,7 @@ import de.gupta.clean.crud.implementation.examples.task.domain.model.dto.TaskDom
 import de.gupta.clean.crud.template.domain.mapping.fetch.DomainResponseBuilder;
 import de.gupta.clean.crud.template.domain.mapping.save.DomainModelBuilder;
 import de.gupta.clean.crud.template.domain.service.crud.policy.InsertionPolicy;
-import de.gupta.clean.crud.template.domain.service.equality.DomainEqualityPolicy;
+import de.gupta.clean.crud.template.domain.service.equality.DuplicateDefinition;
 import de.gupta.clean.crud.template.domain.service.security.DomainSecurityPolicy;
 import de.gupta.clean.crud.template.useCases.crud.save.application.service.AbstractSaveService;
 import de.gupta.clean.crud.template.useCases.crud.save.application.service.SavePersistenceService;
@@ -23,9 +23,9 @@ final class TaskSaveService extends
 					final DomainResponseBuilder<TaskDomainModel, TaskDomainModelResponse> responseModelMapper,
 					final InsertionPolicy<TaskDomainModel> insertionPolicy,
 					final DomainSecurityPolicy<TaskDomainModel> domainSecurityPolicy,
-					final DomainEqualityPolicy<TaskDomainModel> domainEqualityPolicy)
+					final DuplicateDefinition<TaskDomainModel> duplicateDefinition)
 	{
 		super(persistenceService, modelBuilder, responseModelMapper, insertionPolicy, domainSecurityPolicy,
-				domainEqualityPolicy);
+				duplicateDefinition);
 	}
 }
