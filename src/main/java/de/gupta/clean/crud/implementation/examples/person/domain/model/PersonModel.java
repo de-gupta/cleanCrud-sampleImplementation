454d3e27d4eb3b0382101d8cdf7b5c4504ef26ad
de-gupta <de-gupta@>
2026-04-19 00:24:53 +0200

    _masterSatellite: person model extended

--- src/main/java/de/gupta/clean/crud/implementation/examples/person/domain/model/PersonModel.java
+++ src/main/java/de/gupta/clean/crud/implementation/examples/person/domain/model/PersonModel.java
@@ -1,6 +1,10 @@
 package de.gupta.clean.crud.implementation.examples.person.domain.model;
 
+import de.gupta.clean.crud.implementation.examples.note.useCases.crud.common.dto.NoteAPIModelResponse;
+import de.gupta.clean.crud.implementation.examples.version.useCases.crud.common.dto.VersionAPIModelResponse;
+
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Optional;
 
 public interface PersonModel<U, V>
@@ -16,4 +20,10 @@ public interface PersonModel<U, V>
 	Optional<String> lastName();
 
 	LocalDate birthDate();
+
+	Collection<NoteAPIModelResponse> notes();
+
+	VersionAPIModelResponse currentVersion();
+
+	Optional<VersionAPIModelResponse> lastKnownVersion();
 }
