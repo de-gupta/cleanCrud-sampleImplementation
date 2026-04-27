90aea09788ae7d81b934cd6c125047cf33be4fb4
de-gupta <de-gupta@>
2026-04-27 10:59:56 +0200

    _masterSatellite: made rel public

--- src/main/java/de/gupta/clean/crud/implementation/examples/person/domain/model/PersonRelationships.java
+++ src/main/java/de/gupta/clean/crud/implementation/examples/person/domain/model/PersonRelationships.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
-final class PersonRelationships implements Relationships
+public final class PersonRelationships implements Relationships
 {
 	@Override
 	public Class<?> baseModelClass()
